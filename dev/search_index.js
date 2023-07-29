c1f3e4fb7075037d8904b5f408b9819ff07fce02
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 14:35:20 +0000

    build based on b87b9db

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeatPhysics","category":"page"},{"location":"#NeatPhysics","page":"Home","title":"NeatPhysics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeatPhysics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeatPhysics]","category":"page"}]
+}
